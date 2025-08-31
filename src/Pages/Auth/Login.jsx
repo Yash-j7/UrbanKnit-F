177d889d9e2bbc434e24b4b2d95bb38b85fc5586
Yash Kumar Jha <115889463+Yash-j7@users.noreply.github.com>
2025-09-01 02:19:44 +0530

    Update Login.jsx

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -19,7 +19,7 @@ function Login() {
 
     try {
       const res = await axios.post(
-        "https://urnanknit-backend.onrender.com/api/v1/auth/login",
+        "https://urnanknit-backend.onrender.com/auth/login",
         {
           email,
           password,
